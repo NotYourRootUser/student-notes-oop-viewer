1bed94222e126cd40c2f8b1ef714cdc31a6abe1b
Justin <justin.uaisele@gmail.com>
2026-04-14 16:52:21 +1200

    feat: add raw student note dataset

---
+++ app/data.php
@@ -0,0 +1,34 @@
+<?php
+
+$studentNotes = [
+    [
+        "student_name" => "Mia",
+        "note_text" => "Needs more practice with PHP loops and arrays.",
+        "status" => "open"
+    ],
+    [
+        "student_name" => "Andy",
+        "note_text" => "Completed variables and conditionals exercises well.",
+        "status" => "closed"
+    ],
+    [
+        "student_name" => "Mia",
+        "note_text" => "Improved function writing but still needs work on return values.",
+        "status" => "open"
+    ],
+    [
+        "student_name" => "Jordan",
+        "note_text" => "Good understanding of associative arrays and foreach loops.",
+        "status" => "closed"
+    ],
+    [
+        "student_name" => "Chloe",
+        "note_text" => "Struggled with if statements and nested conditions.",
+        "status" => "open"
+    ],
+    [
+        "student_name" => "Andy",
+        "note_text" => "Showed solid progress with reusable helper functions.",
+        "status" => "closed"
+    ],
+];
